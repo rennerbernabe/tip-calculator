6ae0f1833bdae112dea344b2a66e5be4871de7de
rennerbernabe_ <renner.bernabe@cartrack.com>
2024-01-09 12:43:59 +0800

    Add automated test

--- app/src/main/java/com/rbb/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/rbb/tipcalculator/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.annotation.VisibleForTesting
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
@@ -103,7 +104,8 @@ fun TipCalculatorLayout() {
     }
 }
 
-private fun calculateTip(amount: Double, tipPercent: Double = 15.0, roundUp: Boolean): String {
+@VisibleForTesting
+internal fun calculateTip(amount: Double, tipPercent: Double = 15.0, roundUp: Boolean): String {
     var tip = tipPercent / 100 * amount
     if (roundUp) {
         tip = kotlin.math.ceil(tip)
